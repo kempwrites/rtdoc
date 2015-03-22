e957df4294d5ed043486516b9c453787214aa691
kempwrites <kempwrites@comcast.net>
2015-03-22 15:37:45 -0700

    fixed bad code in appdotpy

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ def get_csv():
 	csv_obj = csv.DictReader(csv_file)
 	csv_list = list(csv_obj)
 	return csv_list
-	return render_template(template, object_list=object_list)
 
 
 @app.route("/")
 def index():
 	template = 'index.html'
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list=object_list)
 
 
 if __name__ == '__main__':
