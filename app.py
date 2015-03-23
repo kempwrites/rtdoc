f837d73d27dda93f30db71de85a0e35d1250ee92
kempwrites <kempwrites@comcast.net>
2015-03-22 20:22:34 -0700

    test csv data and link id but code probs

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def get_csv():
 	csv_path = './static/rtfounders.csv'
-	csv_file = open(csv_path, 'rb')
+	csv_file = open(csv_path, 'r')
 	csv_obj = csv.DictReader(csv_file)
 	csv_list = list(csv_obj)
 	return csv_list
@@ -19,13 +19,13 @@ def index():
 	return render_template(template, object_list=object_list)
 
 
-	@app.route('/<row_id>/')
-	def detail(row_id):
-		template = 'detail.html'
-		object_list = get_csv()
-		for row in object_list:
-			if row['id'] == row_id:
-				return render_template(template, object=row)
+@app.route('/<row_id>/')
+def detail(row_id):
+	template = 'detail.html'
+	object_list = get_csv()
+	for row in object_list:
+		if row['id'] == row_id:
+			return render_template(template, object=row)
 
 
 if __name__ == '__main__':
